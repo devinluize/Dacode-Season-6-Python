ec513eee7ff57737e7a3000811411d2616f18ead
Sherry-21 <109456505+Sherry-21@users.noreply.github.com>
2024-08-18 13:56:09 +0700

    Create main.py

---
+++ session 6/main.py
@@ -0,0 +1,71 @@
+# mylist = ["Anton", "devin", "celine", "elys"]
+# print(mylist)
+
+# mylist2 = list(['Anto', 'devin'])
+# print(mylist2)
+
+# newlist = ["angka", 100, "desimal", 10.5]
+# print(newlist)
+
+# mylist = ["Anton", "devin", "celine", "elys"]
+# # index d python itu d mulai dari 0
+# print(mylist[0])
+
+# print(mylist[0:2])
+# #reverse
+# print(mylist[::-1])
+
+# mylist = ["Anton", "devin", "celine", "elys"]
+# mylist2 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+#insert
+# mylist.insert(1, "ryanlie")
+# print(mylist)
+
+#append
+# mylist.append("liee")
+# print(mylist)
+
+#extend
+# mylist.extend(mylist2[0:5])
+# print(mylist)
+
+# + operator
+# list3 = mylist + mylist2
+# print(list3)
+
+# mylist = ["Anton", "devin", "celine", "elys"]
+#clear
+# mylist.clear()
+# print(mylist)
+
+#remove
+# mylist.remove("Anton")
+# print(mylist)
+
+#pop
+# mylist.pop(0)
+# print(mylist)
+
+# mylist = ["Anton", "devin", "celine", "elys"]
+
+#for each
+# for i in mylist :
+#     if i == "Anton":
+#         print("ini adalah Anton")
+#     else:
+#         print("ini bukan Anton")
+
+# for i in range(len(mylist)):
+#     print(mylist[i])
+
+# newlist = [x for x in mylist if x == "Anton"]
+
+# print(newlist)
+
+# mylist = [100, 600, 50, 2, 4, 6, 10, 9, 6, 7, 11]
+
+# mylist.sort()
+# print(mylist)
+
+# mylist.sort(reverse = True)
+# print(mylist)
